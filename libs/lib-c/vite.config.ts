37e9a760d54498ea303e27cecee937be01cb224c
John Walley <john@walley.org.uk>
2025-10-03 15:04:39 +0100

    Emit types for published package

--- libs/lib-c/vite.config.ts
+++ libs/lib-c/vite.config.ts
@@ -1,8 +1,9 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { defineConfig } from 'vite'
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import dts from "vite-plugin-dts";
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
     build: {
@@ -13,4 +14,5 @@ export default defineConfig({
             fileName: 'lib-c',
         },
     },
+    plugins: [dts({ rollupTypes: true, entryRoot: "src", outDir: "dist", insertTypesEntry: true })]
 })
